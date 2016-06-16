c198694861e74dc1cb1f47bab57bc2c42f13a4ea
geminscs <geminscs@gmail.com>
2016-06-16 17:35:43 +0900

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616064203) do
+ActiveRecord::Schema.define(version: 20160616075217) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20160616064203) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "password_digest"
+    t.string   "country"
+    t.string   "city"
+    t.string   "phone"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.delete_all
+Article.create(title: "Sample Article 1", content: "This is the content of sample article 1 ", user_id: 1)
+Article.create(title: "Sample Article 2", content: "This is the content of sample article 2 ", user_id: 2)
