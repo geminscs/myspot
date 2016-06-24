16ee3d02ee92edff44855e85ec491df8992ae024
geminscs <geminscs@gmail.com>
2016-06-24 15:24:57 +0900

    modify css

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,14 +4,14 @@ class SessionController < ApplicationController
     @user = User.find_by_user_name(params[:user_name])
     if @user and @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to home_url
+      redirect_to :back
     else
-      redirect_to home_url
+      redirect_to :back
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to home_url
+    redirect_to :back
   end
 end
